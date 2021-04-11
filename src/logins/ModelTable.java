5736ac155e9bbe8c3b6b17bb8834f2970bd1f2d4
[Max <[rankinsm@wit.edu]>
2021-04-11 19:10:21 -0400

    updated - View Logins functional

---
+++ src/logins/ModelTable.java
@@ -0,0 +1,132 @@
+package logins;
+
+public class ModelTable {
+	String name;
+	String account;
+	String password;
+	int isShared;
+	int sharedW0;
+	int sharedW1;
+	int sharedW2;
+	int sharedW3;
+	int sharedW4;
+	int sharedW5;
+	int sharedW6;
+	int sharedW7;
+	
+	public ModelTable(String name, String account, String password, int isShared, int sharedW0, int sharedW1,
+			int sharedW2, int sharedW3, int sharedW4, int sharedW5, int sharedW6, int sharedW7) {
+		super();
+		this.name = name;
+		this.account = account;
+		this.password = password;
+		this.isShared = isShared;
+		this.sharedW0 = sharedW0;
+		this.sharedW1 = sharedW1;
+		this.sharedW2 = sharedW2;
+		this.sharedW3 = sharedW3;
+		this.sharedW4 = sharedW4;
+		this.sharedW5 = sharedW5;
+		this.sharedW6 = sharedW6;
+		this.sharedW7 = sharedW7;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAccount() {
+		return account;
+	}
+
+	public void setAccount(String account) {
+		this.account = account;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public int getIsShared() {
+		return isShared;
+	}
+
+	public void setIsShared(int isShared) {
+		this.isShared = isShared;
+	}
+
+	public int getSharedW0() {
+		return sharedW0;
+	}
+
+	public void setSharedW0(int sharedW0) {
+		this.sharedW0 = sharedW0;
+	}
+
+	public int getSharedW1() {
+		return sharedW1;
+	}
+
+	public void setSharedW1(int sharedW1) {
+		this.sharedW1 = sharedW1;
+	}
+
+	public int getSharedW2() {
+		return sharedW2;
+	}
+
+	public void setSharedW2(int sharedW2) {
+		this.sharedW2 = sharedW2;
+	}
+
+	public int getSharedW3() {
+		return sharedW3;
+	}
+
+	public void setSharedW3(int sharedW3) {
+		this.sharedW3 = sharedW3;
+	}
+
+	public int getSharedW4() {
+		return sharedW4;
+	}
+
+	public void setSharedW4(int sharedW4) {
+		this.sharedW4 = sharedW4;
+	}
+
+	public int getSharedW5() {
+		return sharedW5;
+	}
+
+	public void setSharedW5(int sharedW5) {
+		this.sharedW5 = sharedW5;
+	}
+
+	public int getSharedW6() {
+		return sharedW6;
+	}
+
+	public void setSharedW6(int sharedW6) {
+		this.sharedW6 = sharedW6;
+	}
+
+	public int getSharedW7() {
+		return sharedW7;
+	}
+
+	public void setSharedW7(int sharedW7) {
+		this.sharedW7 = sharedW7;
+	}
+
+	
+	
+}
--- src/logins/loginsHomeController.java
+++ src/logins/loginsHomeController.java
@@ -2,8 +2,14 @@ package logins;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ResourceBundle;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -13,12 +19,16 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class loginsHomeController implements Initializable {
 
 	public static String[] profileNames = new String[8];
+	private int aID = accounts.accountsLoginController.accountIDNum;
+	private int pID = application.profileLoginController.profileID;
 	
     @FXML
     private Button btnBack;
@@ -37,45 +47,50 @@ public class loginsHomeController implements Initializable {
 
     @FXML
     private Button btn_addLogin;
-
-    @FXML
-    private TableColumn<?, ?> clm_name;
-
-    @FXML
-    private TableColumn<?, ?> clm_user;
-
-    @FXML
-    private TableColumn<?, ?> clm_pass;
-
-    @FXML
-    private TableColumn<?, ?> clm_shared;
-
-    @FXML
-    private TableColumn<?, ?> clm_profile0;
-
-    @FXML
-    private TableColumn<?, ?> clm_profile1;
-
-    @FXML
-    private TableColumn<?, ?> clm_profile2;
-
-    @FXML
-    private TableColumn<?, ?> clm_profile3;
-
-    @FXML
-    private TableColumn<?, ?> clm_profile4;
-
-    @FXML
-    private TableColumn<?, ?> clm_profile5;
-
-    @FXML
-    private TableColumn<?, ?> clm_profile6;
-
-    @FXML
-    private TableColumn<?, ?> clm_profile7;
     
     @FXML
-    private TableColumn<?, ?> clm_button;
+    private TableView<ModelTable> table_logins;
+
+    @FXML
+    private TableColumn<ModelTable, String> clm_name;
+
+    @FXML
+    private TableColumn<ModelTable, String> clm_user;
+
+    @FXML
+    private TableColumn<ModelTable, String> clm_pass;
+
+    @FXML
+    private TableColumn<ModelTable, Integer> clm_shared;
+
+    @FXML
+    private TableColumn<ModelTable, Integer> clm_profile0;
+
+    @FXML
+    private TableColumn<ModelTable, Integer> clm_profile1;
+
+    @FXML
+    private TableColumn<ModelTable, Integer> clm_profile2;
+
+    @FXML
+    private TableColumn<ModelTable, Integer> clm_profile3;
+
+    @FXML
+    private TableColumn<ModelTable, Integer> clm_profile4;
+
+    @FXML
+    private TableColumn<ModelTable, Integer> clm_profile5;
+
+    @FXML
+    private TableColumn<ModelTable, Integer> clm_profile6;
+
+    @FXML
+    private TableColumn<ModelTable, Integer> clm_profile7;
+    
+    @FXML
+    private TableColumn<ModelTable, Button> clm_button;
+    
+    ObservableList<ModelTable> oblist = FXCollections.observableArrayList();
     
     @FXML
     void showAccountHome(ActionEvent event) throws IOException {
@@ -145,9 +160,7 @@ public class loginsHomeController implements Initializable {
     	}
     }
     
-	@Override
-	public void initialize(URL arg0, ResourceBundle arg1) {
-		setNames();
+    private void setColumnNames() {
 		clm_profile0.setText(profileNames[0]);
 		clm_profile1.setText(profileNames[1]);
 		clm_profile2.setText(profileNames[2]);
@@ -156,6 +169,93 @@ public class loginsHomeController implements Initializable {
 		clm_profile5.setText(profileNames[5]);
 		clm_profile6.setText(profileNames[6]);
 		clm_profile7.setText(profileNames[7]);
+		
+		switch(pID) {
+		case 0:
+			clm_profile0.setText("You");
+		case 1:
+			clm_profile1.setText("You");
+		case 2:
+			clm_profile2.setText("You");
+		case 3:
+			clm_profile3.setText("You");
+		case 4:
+			clm_profile4.setText("You");
+		case 5:
+			clm_profile5.setText("You");
+		case 6:
+			clm_profile6.setText("You");
+		case 7:
+			clm_profile7.setText("You");
+		}
+    }
+    
+    private String getUser() {
+    	if(pID == 0) {
+    		return "sharedProf0 ";
+    	}
+    	else if(pID == 1) {
+    		return "sharedProf1 ";
+    	}
+    	else if(pID == 2) {
+    		return "sharedProf2 ";
+    	}
+    	else if(pID == 3) {
+    		return "sharedProf3 ";
+    	}
+    	else if(pID == 4) {
+    		return "sharedProf4 ";
+    	}
+    	else if(pID == 5) {
+    		return "sharedProf5 ";
+    	}
+    	else if(pID == 6) {
+    		return "sharedProf6 ";
+    	}
+    	else if(pID == 7) {
+    		return "sharedProf7 ";
+    	}
+    	return null;
+    }
+    
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		setNames();
+		setColumnNames();
+		
+		String sharedProf = getUser();
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");  
+			Connection con;
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/lockpick","root","rootPass");
+			Statement stmt = con.createStatement(); 
+			ResultSet rs = stmt.executeQuery("select * FROM tablelogins WHERE accountID = '"+aID+"' AND profileID = '"+pID+"' OR "+sharedProf+" = '1';");
+			while(rs.next()) {
+				oblist.add(new ModelTable(rs.getString("loginName"), rs.getString("loginUser"), rs.getString("loginPassword"), 
+						rs.getInt("isSharable"), rs.getInt("sharedProf0"), rs.getInt("sharedProf1"), rs.getInt("sharedProf2"),
+						rs.getInt("sharedProf3"), rs.getInt("sharedProf4"), rs.getInt("sharedProf5"), rs.getInt("sharedProf6"), rs.getInt("sharedProf7")));
+			}
+
+		} 
+		catch (Exception e) {
+			System.out.println(e);
+		}
+		
+		clm_name.setCellValueFactory(new PropertyValueFactory<>("name"));
+		clm_user.setCellValueFactory(new PropertyValueFactory<>("account"));
+		clm_pass.setCellValueFactory(new PropertyValueFactory<>("password"));
+		clm_shared.setCellValueFactory(new PropertyValueFactory<>("isShared"));
+		clm_profile0.setCellValueFactory(new PropertyValueFactory<>("sharedW0"));
+		clm_profile1.setCellValueFactory(new PropertyValueFactory<>("sharedW1"));
+		clm_profile2.setCellValueFactory(new PropertyValueFactory<>("sharedW2"));
+		clm_profile3.setCellValueFactory(new PropertyValueFactory<>("sharedW3"));
+		clm_profile4.setCellValueFactory(new PropertyValueFactory<>("sharedW4"));
+		clm_profile5.setCellValueFactory(new PropertyValueFactory<>("sharedW5"));
+		clm_profile6.setCellValueFactory(new PropertyValueFactory<>("sharedW6"));
+		clm_profile7.setCellValueFactory(new PropertyValueFactory<>("sharedW7"));
+		
+		table_logins.setItems(oblist);
 	}
 
 }
